53154a80f0b8862a7aec094c94a132a7c07bb3aa
waris <warisamjed@gmail.com>
2025-12-30 12:57:58 +0100

    chore:update configuration files

--- dotfiles/config/common/.config/fish/config.fish
+++ dotfiles/config/common/.config/fish/config.fish
@@ -2,6 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set -gx PATH /usr/bin /bin $PATH
+set -gx EDITOR nvim
+set -gx VISUAL nvim
 
 # ===============================
 # Gruvbox Dark colors
